fd87c356a41f4d59725516b5471cf2e67e08f2f1
TheTanishqChauhan <155085790+thetanishqchauhan@users.noreply.github.com>
2024-02-27 23:08:23 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,3 +1,3 @@
-import pandas as pd
-diabetes = pd.read_csv('https://github.com/YBIFoundation/Dataset/raw/main/Diabetes.csv')
-diabetes.head()
+#import pandas as pd
+#diabetes = pd.read_csv('https://github.com/YBIFoundation/Dataset/raw/main/Diabetes.csv')
+#diabetes.head()
