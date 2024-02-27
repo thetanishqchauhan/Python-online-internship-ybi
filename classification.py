87c7b58d059b7a4f9c9263d4de6bfe517e1aa8a2
thetanishqchauhan <thisistanishqchauhan@gmail.com>
2024-02-27 23:08:07 +0530

    first commit

---
+++ classification.py
@@ -0,0 +1,3 @@
+import pandas as pd
+diabetes = pd.read_csv('https://github.com/YBIFoundation/Dataset/raw/main/Diabetes.csv')
+diabetes.head()
---
+++ file1-project.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+salary = pd.read_csv('https://github.com/YBIFoundation/Dataset/raw/main/Salary%20Data.csv')
+salary.head()
